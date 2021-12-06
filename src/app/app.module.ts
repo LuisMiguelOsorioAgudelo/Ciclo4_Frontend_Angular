444989817e4164192779ca37a19294a42aa40924
LuisMiguelOsorioAgudelo <osoriomiguel81@gmail.com>
2021-12-06 10:37:36 -0500

    Clase jueves y viernes semana5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { FooterComponent } from './assets/footer/footer.component';
 import { NavbarComponent } from './assets/navbar/navbar.component';
 import { IndexComponent } from './assets/index/index.component';
 import { ErrorComponent } from './assets/error/error.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -18,6 +19,7 @@ import { ErrorComponent } from './assets/error/error.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
